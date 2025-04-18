4d4725ddfec97e5e40fbf2f46e0c85a955e2ea43
JustFossa <panko.marek@icloud.com>
2025-04-18 20:57:10 +0200

    init

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { FaGithub } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <div className="w-[50%]">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl font-bold text-left">
+          IP Info
+        </h1>
+        <p className="text-left text-2xl">
+          A powerful set of tools to get information about IP addresses.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ components/theme-provider.tsx
@@ -0,0 +1,8 @@
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import React from "react";
+
+export default function ThemeProvider({
+    children, ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
+    return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
